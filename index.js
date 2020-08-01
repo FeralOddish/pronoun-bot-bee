a40dd0fe038f60e9b0488dab95626a3d175ec377
FeralOddish <sittal.m@gmail.com>
2020-07-31 22:23:43 -0500

    bot can assign they/them

--- index.js
+++ index.js
@@ -9,12 +9,34 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+permissibleRoles = ["they/them"]
+
+function isPermissibleRole(roleName) {
+  var found=false
+  // for (var i=0; i<permissibleRoles.length; i++ ) {
+  //   var thisrole = permissibleRoles[i]
+  //   found=(thisrole.equals(roleName))
+  //   if (found) break;
+  // }
+  // var i=0;
+  // while (!found && i<permissibleRoles.length) {
+  //   var thisrole = permissibleRoles[i];
+  //   found=(thisrole.equals(roleName));
+  //   i++;
+  // }
+  // return found
+  return permissibleRoles.some(thisRole => thisRole.equals(roleName));
+}
+
 bot.on('message', msg => {
   if (msg.content.search("!iam")>=0) {
     message = msg.content;
     mt = message.match(/!iam ([\w/]+)/);
-    rolename=mt[1];
-    var role= member.guild.roles.cache.find(role => role.name === rolename);
-    member.roles.add(role);
+    roleName=mt[1];
+    if (permissibleRoles.some(thisRole => (thisRole===roleName))) {
+      var roles = msg.guild.roles;
+      var role = msg.guild.roles.find(role => role.name === roleName);
+      if (role) msg.member.addRole(role);
+    }
   }
 });
